927ee8c943634ef2139a2bf70936b0483cd0ba38
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-21 15:30:22 +0100

    Added Session class to save who is currently connected in the app

---
+++ TheAmazingExcavatorRentTool/Services/Session.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace TheAmazingExcavatorRentTool.Services
+{
+    public class Session
+    {
+        private static Int32 _id;
+        private static string _username;
+        private static string _password;
+        private static bool _isAdmin;
+        public static Int32 Id
+        {
+            get 
+            { 
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static bool IsAdmin { get; set; }
+
+
+        public static void ShowSessionInfos()
+        {
+            Console.WriteLine("session user id: "+ Id);
+            Console.WriteLine("session username: "+ Username);
+            Console.WriteLine("session user password: "+ Password);
+            Console.WriteLine("session user admin: "+ IsAdmin);
+        }
+
+        public static void SessionClear()
+        {
+            Id = Convert.ToInt32(null);
+            Username = null;
+            Password = null;
+            IsAdmin = Convert.ToBoolean(null);
+        }
+    }
+}
