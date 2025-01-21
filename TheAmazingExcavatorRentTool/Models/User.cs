2d2d3d8308b756b810dcd4c0760f4fc9730f0aee
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-21 15:13:48 +0100

    Added user model

---
+++ TheAmazingExcavatorRentTool/Models/User.cs
@@ -0,0 +1,19 @@
+namespace DVDRENT.Models
+{
+    public class User
+    {
+        internal int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User(int userid, string username, string password, bool isAdmin)
+        {
+            UserId = userid;
+            Username = username;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+        
+    }
+}
