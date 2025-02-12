208fcd880c870fe042da6fc713e405d38faf4db6
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-02-12 21:24:45 +0100

    - Added update customer method

--- TheAmazingExcavatorRentTool/ViewModels/CustomerVM.cs
+++ TheAmazingExcavatorRentTool/ViewModels/CustomerVM.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Windows;
 using MySqlConnector;
 using TheAmazingExcavatorRentTool.Models;
 using TheAmazingExcavatorRentTool.Services;
@@ -22,6 +23,8 @@ public class CustomerVM: BaseVM
     {
         // Creating queries string
         loadQuery = "SELECT customer_id, first_name, last_name, email, birth_date FROM customer";
+        updateQuery =
+            "UPDATE customer SET first_name=@first_name, last_name=@last_name, email=@email, birth_date=@birth_date WHERE customer_id=@id";
         
         Load();
     }
@@ -80,10 +83,6 @@ public class CustomerVM: BaseVM
     // public DelegateCommand<Excavator> DeleteCommand =>
     //     _deleteCommand ?? (_deleteCommand = new DelegateCommand<Excavator>(DeleteExcavator));
     //
-    // private DelegateCommand<Excavator> _updateCommand;
-    // public DelegateCommand<Excavator> UpdateCommand =>
-    //     _updateCommand ?? (_updateCommand = new DelegateCommand<Excavator>(UpdateExcavator));
-    //
     // private DelegateCommand _addCommand;
     // public DelegateCommand AddCommand =>
     //     _addCommand ?? (_addCommand = new DelegateCommand(AddExcavator));
@@ -120,5 +119,54 @@ public class CustomerVM: BaseVM
         dbCon.Close();
 
         Customers = customers;
+    }
+
+
+    private void Update(Customer customer_to_update)
+    {
+        // Check if a customer already exists with the same first name, last name and email
+        foreach (var customer in Customers)
+        {
+            // if (customer.CustomerId == customer_to_update.CustomerId)
+            //     continue;
+            if (customer.FirstName == customer_to_update.FirstName && customer.LastName == customer_to_update.LastName && customer.Email == customer_to_update.Email && customer.CustomerId != customer_to_update.CustomerId)
+            {
+                soundPlayer.PlayFailSound();
+                MessageBox.Show("Une un client de même nom avec la même adresse email éxiste déjà!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+        // Updating customer in database
+        var dbCon = getDbCon();
+        
+        if (!dbCon.IsConnect())
+        {
+            Console.WriteLine("Cannot connect to database (maybe MySql server isn't running!)");
+            throw new Exception(); //todo creer exception custom (style FailedConnectionException)
+        }
+        
+        var cmd = new MySqlCommand(updateQuery, dbCon.Connection);
+        cmd.Parameters.AddWithValue("@first_name", customer_to_update.FirstName);
+        cmd.Parameters.AddWithValue("@last_name", customer_to_update.LastName);
+        cmd.Parameters.AddWithValue("@email", customer_to_update.Email);
+        cmd.Parameters.AddWithValue("@birth_date", customer_to_update.BirthDate);
+        cmd.Parameters.AddWithValue("@id", customer_to_update.CustomerId);
+        cmd.ExecuteReader();
+        dbCon.Close();
+        
+        // Updating user in app
+        for (int i = 0; i < Customers.Count; i++)
+        {
+            if (Customers[i].CustomerId == customer_to_update.CustomerId)
+            {
+                Customers[i] = customer_to_update;
+            }
+        }
+        
+        // Notify the user that the update succeeded
+        soundPlayer.PlaySuccessSound();
+        MessageBox.Show("Modifications appliquées à la pelleteuse", "Modifications Appliquées", MessageBoxButton.OK,
+            MessageBoxImage.Information);
+        
     }
 }
