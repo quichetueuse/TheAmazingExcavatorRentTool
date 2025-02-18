11782eea9ceaef0f075d9fa559092a7f5c0a65af
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-02-18 12:38:03 +0100

    - Added GeneratePassword method

--- TheAmazingExcavatorRentTool/Services/PasswordManager.cs
+++ TheAmazingExcavatorRentTool/Services/PasswordManager.cs
@@ -1,18 +1,12 @@
 using System.Text;
-
+using System.Security.Cryptography;
 namespace TheAmazingExcavatorRentTool.Services;
+using System.Linq;
 
 public class PasswordManager
 {
-    public PasswordManager()
-    {
-        int password_length = 14;
-        int min_num = 2;
-        int min_lower = 2;
-        int min_upper = 2;
-        int min_special = 2;
-    }
-
+    private const string AlphanumericChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private const string SpecialChars = "!@#$%^&*()-_=+[]{};:,.<>?";
 
     internal string HashPassword(string password)
     {
@@ -21,8 +15,31 @@ public class PasswordManager
         return Convert.ToBase64String(hash);
     }
     
-    public string GeneratePassword()
+    public string GeneratePassword(int length, int numberOfNonAlphanumericCharacters)
     {
-        return "";
+        
+        if (length < numberOfNonAlphanumericCharacters)
+            throw new ArgumentException("Password length is lower than minimum characters given");
+
+        var rng = new RNGCryptoServiceProvider();
+        char[] password = new char[length];
+
+        // Générer les caractères alphanumériques
+        for (int i = 0; i < length - numberOfNonAlphanumericCharacters; i++)
+            password[i] = AlphanumericChars[GetRandomNumber(rng, AlphanumericChars.Length)];
+
+        // Générer les caractères spéciaux
+        for (int i = length - numberOfNonAlphanumericCharacters; i < length; i++)
+            password[i] = SpecialChars[GetRandomNumber(rng, SpecialChars.Length)];
+
+        // Mélanger le mot de passe
+        return new string(password.OrderBy(_ => GetRandomNumber(rng, length)).ToArray());
+    }
+    
+    private static int GetRandomNumber(RNGCryptoServiceProvider rng, int max)
+    {
+        byte[] randomNumber = new byte[4];
+        rng.GetBytes(randomNumber);
+        return (int)(BitConverter.ToUInt32(randomNumber, 0) % max);
     }
 }
